65770ad3024356e70e306966878246bbd72ed7ab
ASEUdev <88355964+ASEUdev@users.noreply.github.com>
2023-06-26 08:22:53 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -7,17 +7,49 @@
             text-align: center;
         }
     </style>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+     integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" 
+     crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="index.css" rel="stylesheet" type="text/css" >
 </head>
 <body>
+    
     <h1>Menú Principal</h1>
-    <div>
-        <a href="sanciones.php">Sanciones</a>
-    </div>
-    <div>
-        <a href="multas.php">Multas</a>
-    </div>
-    <div>
-        <a href="otra_opcion.php">Otra Opción</a>
-    </div>
+        <div class="contenedor">
+
+<div class="card">
+    <h2><a href="sanciones/sanciones.php" style="color:rgb(240, 255, 255);">Sanciones</a></h2>
+    <p>aqui podras ver las sanciones con el repectivo numero de siri</p>
+    <span>hover</span>
+    <div class="pic"></div>
+    <button></button>
+</div>
+                <div class="card card2">
+
+                            <h2><a href="multas/multas.php" style="color:rgb(240, 255, 255);">Multas</a></h2>
+                            <p>aqui podras ver las multas con la placa del automotor</p>
+                            <span>hover</span>
+                            <div class="pic"></div>
+                            <button></button>
+                        
+                </div>
+                    
+                <!-- <div class="card">
+                        
+                           <h2><a href="otra_opcion.php">Otra Opción</a></h2>
+                           <p>aqui lo que vaya XD</p>
+                           <span>hover</span>
+                           <div class="pic"></div>
+                           <button></button>
+                       
+                </div> -->
+           
+        </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+    <script src="index.js" ></script>
 </body>
 </html>
